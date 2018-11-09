db36ed41f122809fa61e11ca9a255cb70dccebea
proud-entropy-9548 <e.e.seieroe@gmail.com>
2018-11-09 22:31:00 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 def square_array(array)
-  array=[1,2,3]
-  new_array << array.each{ |n| n**2} 
   
+  
+  new_array = []
+  
+  array.each do |number|
+    new_array << number**2
+  end
+   
+  return new_array
 end
